56ead575fad7b26e4d46d7774a7cbc0c7e817c54
Rodrigo Martins de Freitas Alves <rodrigo.martins.50@hotmail.com>
2019-11-06 22:35:06 -0300

    Incluindo useDelete e usePost.

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 import useGet from './useGet'
+import usePost from './usePost'
+import useDelete from './useDelete'
 
 const url = 'https://mymoney-rodrigomartins.firebaseio.com/movimentacoes.json'
 
 function App() {
   const data = useGet(url)
+  const [postData, post] = usePost(url)
+  const [deleteData, remove] = useDelete()
+
+  const saveNew = () => {
+    post({valor: 10, descricao:'Olá'})
+  }
+
+  const doRemove = () => {
+    remove ('https://mymoney-rodrigomartins.firebaseio.com/movimentacoes/-Lt2CYtrF6o06OOFZDum.json')
+  }
 
   return (
     <div>
@@ -15,6 +27,11 @@ function App() {
         data.loading &&
         <div>Loading</div>
       }
+
+      <button onClick={saveNew}>Salvar</button>
+      <pre>{JSON.stringify(postData)}</pre>
+      <button onClick={doRemove}>Delete</button>
+      <pre>{JSON.stringify(deleteData)}</pre>
     </div>
   );
 }
