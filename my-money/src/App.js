c305f6dff814b9c5ca85834bc84bc120817791d3
Rodrigo Martins de Freitas Alves <rodrigo.martins.50@hotmail.com>
2019-11-08 23:36:05 -0300

    Separando componentes.

--- my-money/src/App.js
+++ my-money/src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Rest from './rest'
 
+import Header from './elements/Header'
+import Meses from './Meses'
+import AdicionarMes from './AdicionarMes';
+
+
 const baseUrl = 'https://mymoney-rodrigomartins.firebaseio.com/'
 
 const {useGet, usePost, useDelete} = Rest(baseUrl)
@@ -21,62 +26,11 @@ function App() {
 
   return (
     <div>
-      <nav className='navbar navbar-light bg-light'>
-        <div className='container'>
-          <a className='navbar-brand'>MyMoney</a>
-        </div>
-      </nav>
+      <Header />
 
-      <div className='container'>
-        <h2>Adicionar mês</h2>
-        <select>
-          <option value='2019'>2019</option>
-          <option value='2020'>2020</option>
-        </select>
-        <select>
-          <option value='01'>01</option>
-          <option value='02'>02</option>
-        </select>
-        <button>Adicionar mês</button>
-
-        {
-          data.loading && <span>Carregando...</span>
-        }
-        {
-          !data.loading && (
-            <table className='table'>
-              <thead>
-                <tr>
-                  <th>Mês</th>
-                  
-                  <th>Previsão entrada</th>
-                  <th>Entrada</th>
-
-                  <th>Previsão saída</th>
-                  <th>Saída</th>
-                </tr>
-              </thead>
-              <tbody>
-                {
-                  Object
-                    .keys(data.data)
-                    .map(mes => {
-                      return (
-                        <tr key={mes}>
-                          <td>{mes}</td>
-                          <td>{data.data[mes].previsao_entrada}</td>
-                          <td>{data.data[mes].entrada}</td>
-                          <td>{data.data[mes].previsao_saida}</td>
-                          <td>{data.data[mes].saida}</td>
-                        </tr>
-                      )
-                    })
-                }
-                
-              </tbody>
-            </table>
-          )
-        }
+      <div className='container'>        
+        <AdicionarMes />
+        <Meses />
       </div>
       {JSON.stringify(data)}
     </div>
